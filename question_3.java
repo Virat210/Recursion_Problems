878e0ff95ead4d68c27519e214d57256bdf3f1e9
VIRAT SINGH <viratchauhan1010@gmail.com>
2026-08-26 18:37:04 +0530

    Correct factorial function implementation
    Fix factorial function to return correct base case value and recursive call.

--- question_3.java
+++ question_3.java
@@ -3,8 +3,8 @@
 
 public class Factorial {
     static int factorial(int n) {
-        if (n == 0 || n == 1) return 0;
-        return n * factorial(n - 1);
+        if (n == 0 || n == 1) return 1;
+        return n * factorial(n );
     }
 
     public static void main(String[] args) {
