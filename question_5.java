c14874af92eb922f09c175e4e572025847891ad6
VIRAT SINGH <viratchauhan1010@gmail.com>
2026-09-01 19:26:37 +0530

    Update question_5.java

--- question_5.java
+++ question_5.java
@@ -9,7 +9,7 @@ public class ArrayMaxMin {
 
     static int findMin(int[] arr, int n) {
         if (n == 1) return arr[0];
-        return Math.min(arr[n - 2], findMin(arr, n - 1));
+        return Math.min(arr[n - 1], findMin(arr, n - 1));
     }
 
     public static void main(String[] args) {
