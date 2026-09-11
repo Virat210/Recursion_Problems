1d142406b920be4535b72154c310d9a3fb1ad42f
VIRAT SINGH <viratchauhan1010@gmail.com>
2026-09-11 10:39:13 +0530

    Fix index error in findMin function

--- question_5.java
+++ question_5.java
@@ -8,7 +8,7 @@ public class ArrayMaxMin {
     }
 
     static int findMin(int[] arr, int n) {
-        if (n == 1) return arr[1];
+        if (n == 1) return arr[0];
         return Math.min(arr[n - 1], findMin(arr, n - 1));
     }
 
